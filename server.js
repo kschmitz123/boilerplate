95542504677e34fe85fe0492182bc266cf46c04c
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-21 15:03:04 +0100

    Add heroku config

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -9,4 +10,15 @@ app.get("/", (request, response) => {
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
