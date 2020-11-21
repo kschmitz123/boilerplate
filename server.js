bda4ba84c7e58c7763983366cce94239797ce2b9
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-21 15:57:06 +0100

    Change order in server.js

--- server.js
+++ server.js
@@ -4,16 +4,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
-
 app.use(express.static(path.join(__dirname, "client/build")));
-
 app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
@@ -21,4 +12,7 @@ app.use(
 
 app.get("*", (request, response) => {
   response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
